628c7dd07b8fdb2840086237fb33935689100cc7
tarkeztarkez <szyda47@gmail.com>
2020-07-23 19:26:31 +0200

    Data działa

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from jira import JIRA
+import re
+import dateutil.parser
+import datetime
 
 options = {
     "cookies":{
@@ -16,10 +19,10 @@ block_num = 0
 while True:
     start_idx = block_num*block_size
     issues = jira.search_issues\
-        ("status = Done AND 'Story Points' is not EMPTY AND project != 'Zakupy Fenige' AND project != Urlopy ",
+        ("status = Done AND 'Story Points' is not EMPTY AND project != 'Zakupy Fenige' AND project != Urlopy AND resolved > startOfMonth(-6)",
          start_idx,
          block_size,
-         fields="Story Points,project,resolutiondate")
+         fields="Story Points,project,resolutiondate,customfield_10004")
     if len(issues) == 0:
         # Retrieve issues until there are no more to come
         break
@@ -31,8 +34,17 @@ while True:
         toWrite.append(str(issue.fields.project))
         toWrite.append(str(getattr(issue.fields,nameMap["Story Points"])).replace(".",","))
 
+        sname = ""
+        try:
+            for sprint in issue.fields.customfield_10004:
+                sprint_name = str(re.findall(r"name=[^,]*", str(issue.fields.customfield_10004[0])))
+                sname = sprint_name.lstrip("['name=").rstrip("']")
+        except:pass
+        toWrite.append(sname)
 
-
+        date = dateutil.parser.parse(issue.fields.resolutiondate)
+        date = date.strftime("%b %Y")
+        toWrite.append(date)
 
         print(";".join(toWrite))
         file.write(";".join(toWrite)+"\n")
